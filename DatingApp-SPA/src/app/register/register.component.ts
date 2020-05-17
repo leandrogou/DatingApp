b71d434835b6ff4edec758c577883aae974c72e8
leandrogou <leandro.gou@live.com>
2020-05-17 10:54:15 -0300

    Fim da sessão 6

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +11,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -18,9 +19,9 @@ export class RegisterComponent implements OnInit {
   register()
   {
      this.authService.register(this.model).subscribe(() => {
-       console.log('registrado com sucesso');
+       this.alertify.success('registrado com sucesso');
      }, error => {
-       console.log(error);
+       this.alertify.error(error);
      });
   }
 
--- DatingApp.API/Dtos/UserforRegisterDto.cs
+++ DatingApp.API/Dtos/UserforRegisterDto.cs
@@ -4,10 +4,10 @@ namespace DatingApp.API.Dtos
 {
     public class UserforRegisterDto
     {
-        [Required(ErrorMessage="Nome do usuário não pode ser vazio!")]
+        [Required(ErrorMessage="Campo nome do usuário não pode ser vazio!")]
         public string Username { get; set; }
         
-        [Required(ErrorMessage="Senha não pode ser vazio!")]
+        [Required(ErrorMessage="Campo senha não pode ser vazio!")]
         [StringLength(8,MinimumLength=4,ErrorMessage="A senha deve ter entre 4 e 8 caracteres")]
         public string Password { get; set; }
     }
